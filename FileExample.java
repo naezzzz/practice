01e7b6fd297427045d4a872e293a1708f08ef916
naez <wjsgowl0224@naver.com>
2025-01-05 13:15:43 +0900

    first commit

---
+++ FileExample.java
@@ -0,0 +1,54 @@
+package ch18.sec11;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class FileExample {
+
+	public static void main(String[] args) {
+
+		
+		try {
+			File dir1 = new File("C:/Temp/images");
+			File file1 = new File("C:/Temp/file1.txt");
+			File file2 = new File("C:/Temp/file2.txt");
+			File file3 = new File("C:/Temp/file3.txt");
+			
+			if(dir1.exists() == false) {
+				dir1.mkdirs();
+			}
+			if(file1.exists() == false) {
+				file1.createNewFile();
+			}
+			if(file2.exists() == false) {
+				file2.createNewFile();
+			}
+			if(file3.exists() == false) {
+				file3.createNewFile();
+			}
+			
+			File dir2 = new File("C:/Temp");
+			File[] contents = dir2.listFiles();
+			
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd a HH:mm");
+			for(File file : contents) {
+				System.out.printf("%-25s", sdf.format(new Date(file.lastModified())));
+				if(file.isDirectory()) {
+					System.out.printf("%-10s%-20s\n", "<DIR>", file.getName());
+				} else {
+					System.out.printf("%-10s%-20s\n", file.length(), file.getName());
+				}
+
+			}
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
